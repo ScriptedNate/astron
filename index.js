bb6512133ba9f3b3164e6ba4743353fa49601840
ScriptedNate <35863804+ScriptedNate@users.noreply.github.com>
2019-05-27 09:27:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
+bot.login(process.env.BOT_TOKEN)
 bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
