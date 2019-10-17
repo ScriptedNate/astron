b0a2f38945697ba208e2c68bf5ecc784a705694a
ScriptedNate <35863804+ScriptedNate@users.noreply.github.com>
2019-10-17 08:39:53 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -25,7 +25,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("**.uptime**", "Shows uptime for bot")
     .addField("**.info**", "Shows information about the bot")
     .addField("**.rps**", "Rock!, Paper!, Scissors!")
-    .setFooter("Astron created by by @astrovibes#8096");
+    .setFooter("Astron created by by @nate#6747");
 
     message.channel.send(botembed);
 }
