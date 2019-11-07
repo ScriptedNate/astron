d18a0e0b5806f74a1101b1203c29c76b1f479529
ScriptedNate <35863804+ScriptedNate@users.noreply.github.com>
2019-11-07 13:18:38 +0000

    Add files via upload

---
+++ commands/serverlist.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+const botconfig = require("../botconfig.json");
+const colours = require("../colours.json");
+const superagent = require("superagent")
+
+module.exports.run = async (bot, message, args) => {
+    let sEmbed = new Discord.RichEmbed()
+.setDescription("Serverlist")
+.setColor("RANDOM")
+.addField("**Servers:**", `${bot.guilds.size}`, true)
+.setTimestamp()
+.setFooter('Astron', bot.user.displayAvatarURL);
+
+message.channel.send({embed: sEmbed});
+
+}
+
+module.exports.help = {
+    name: "slots"
+}
